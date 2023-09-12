edd87d67110ae2668776c0860a526f84e0ba702b
maciek@arch <mcyran1996@gmail.com>
2023-09-13 01:35:59 +0200

    lesson 96

---
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+  }
+}
